c39df0f898a7f1f1c4b8a1ecae614d3f0dc9356c
Praveen-Chasta <praveen.chastaa@gmail.com>
2024-09-30 16:51:23 +0530

    arr-question

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-const orangeBtn = document.getElementById("orange");
-const redBtn = document.getElementById("red");
+// const orangeBtn = document.getElementById("orange");
+// const redBtn = document.getElementById("red");
 
 
-function clouser(color){
+// function clouser(color){
 
-   return function(){
-        document.body.style.backgroundColor = `${color}`
-   }
+//    return function(){
+//         document.body.style.backgroundColor = `${color}`
+//    }
 
-}
-
-// orangeBtn.onclick = function(){
-//     document.body.style.backgroundColor = "orange"
 // }
 
-// redBtn.onclick = function(){
-//     document.body.style.backgroundColor = "red"
-// }
+// // orangeBtn.onclick = function(){
+// //     document.body.style.backgroundColor = "orange"
+// // }
 
-orangeBtn.addEventListener("click", clouser("orange"));
-redBtn.addEventListener("click", clouser("red"))
+// // redBtn.onclick = function(){
+// //     document.body.style.backgroundColor = "red"
+// // }
+
+// orangeBtn.addEventListener("click", clouser("orange"));
+// redBtn.addEventListener("click", clouser("red"))
+
+
+const h1Element = document.getElementById("h1")
+const array = ["Praveen", "Chasta", "Anuj", "Chasta"]
+let currentIndex = 0
+
+h1Element.addEventListener("click", () => {
+   h1Element.textContent = array[currentIndex]
+
+   currentIndex ++ 
+
+   
+})
